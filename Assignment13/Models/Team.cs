9e76166d3a18cfb76f96b4653b1b145e34cdec3a
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-29 16:07:11 +0530

    first commit

---
+++ Assignment13/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment13.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public string NOWC { get; set; }
+    }
+}
